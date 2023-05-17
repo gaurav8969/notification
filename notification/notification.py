ee7f02e4b7e2d0f7cf8f211c874cc68665d2c6af
Gaurav Tiwari <gauravkrtiwari79@gmail.com>
2023-05-18 01:00:29 +0530

    Added a full-stop to appear proper.

--- notification/notification.py
+++ notification/notification.py
@@ -3,5 +3,5 @@ from notifypy import Notify
 
 notification = Notify()
 notification.title = "Cool Title"
-notification.message = "Even cooler message"
+notification.message = "Even cooler message."
 notification.send()
